37c0c8ada0814f13d610ec0f5b6f5f4ad2bd22b5
StarmaQ <starmaq101@gmail.com>
2019-12-04 15:36:40 +0100

    Update clampVector.lua

--- clampVector.lua
+++ clampVector.lua
@@ -1,3 +1,3 @@
-local function clamp(v3, x, y, z)
+local function clampVector(v3, x, y, z)
 	return Vector3.new(math.clamp(v3.X, x[1], x[2]), math.clamp(v3.Y, y[1], y[2]), math.clamp(v3.Z, z[1], z[2]))
 end
