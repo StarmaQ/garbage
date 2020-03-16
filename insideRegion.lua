5327e23291bd356273a0cbc48a52a75c2b9a492a
StarmaQ <starmaq101@gmail.com>
2020-03-16 14:04:55 +0100

    Update insideRegion.lua

--- insideRegion.lua
+++ insideRegion.lua
@@ -1,14 +1,14 @@
 --a inRegion function which is useful as heck, roblox add please thank you
 
 local NormalIds = Enum.NormalId:GetEnumItems()
-function insideRegion(cf,size,point)
+function insideRegion(region, object)
     for i = 1, #NormalIds do
         local enum = NormalIds[i]
         local norm = Vector3.FromNormalId(enum)
-        local worldnorm = cf:vectorToWorldSpace(norm)
-        local distance = (norm * size/2).Magnitude
-        local planepoint = cf.Position + worldnorm * distance
-        if (point - planepoint):Dot(worldnorm) > 0 then
+        local worldnorm = region.CFrame:vectorToWorldSpace(norm)
+        local distance = (norm * region.Size/2).Magnitude
+        local planepoint = region.CFrame.Position + worldnorm * distance
+        if (object.Position - planepoint):Dot(worldnorm) > 0 then
             -- not inside region
             return false
         end
