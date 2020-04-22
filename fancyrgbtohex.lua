4ae970b79fef3a37395e4f13b88a5e8cadd966aa
StarmaQ <starmaq101@gmail.com>
2020-04-23 00:10:35 +0200

    Update fancyrgbtohex.lua

--- fancyrgbtohex.lua
+++ fancyrgbtohex.lua
@@ -1,1 +1,6 @@
 
+local hex = 0xFFFFFF --example
+local R = bit32.rshift(bit32.band(hex, 0xFF0000), 16)
+local G = bit32.rshift(bit32.band(hex, 0x00FF00), 8)
+local B = bit32.band(hex, 0x0000FF)
+print(R, G, B)
